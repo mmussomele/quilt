081a38272e47e27506028a15566ce3b9c81560cc
Matthew Mussomele <mmussomele@berkeley.edu>
2016-09-13 00:44:06 -0700

    scale: Add swarm support to plot.py

--- quilt-tester/scale/plot.py
+++ quilt-tester/scale/plot.py
@@ -14,8 +14,10 @@ def run_parser():
     parser = argparse.ArgumentParser(description="run the scale tests", usage="%(prog)s run [-h] [-start N] [-factor M]")
     parser.add_argument('--start', default=100, type=int)
     parser.add_argument('--factor', default=1.5, type=float)
+    parser.add_argument('--image', default='mmussomele/sleep', type=str)
     return parser
 
+NAMESPACE = "scale-bd89e4c89f4d384e7afb155a3af99d8a6f4f5a06a9fecf0b6d220eb66e"
 PREBOOT_SPEC = "./plot/preboot-scale.spec"
 BOOT_SPEC = "./plot/scale.spec"
 FULL_MESH_BOOT_SPEC = "./plot/scale-full-mesh.spec"
@@ -31,50 +33,76 @@ scale -preboot-stitch={0}.tmp \
 -log-file={4} \
 -append -nostop \
 """
+SWARM_CMD = """\
+swarm -preboot-stitch={0}.tmp \
+-image={1} \
+-containers={2} \
+-out-file={3} \
+-log-file={4} \
+-append -nostop \
+"""
 
 DEFAULT_CMD = SCALE_CMD.format(PREBOOT_SPEC, BOOT_SPEC, POSTBOOT_SPEC, OUT_FILE + ".disconnect", LOG_FILE)
 FULL_MESH_CMD = SCALE_CMD.format(PREBOOT_SPEC, FULL_MESH_BOOT_SPEC, FULL_MESH_POST_SPEC, OUT_FILE + ".connected", LOG_FILE)
+
+def stop_namespace():
+    quilt = subprocess.Popen(shlex.split("quilt daemon"))
+    quilt_stop = subprocess.Popen(shlex.split("quilt stop {0}".format(NAMESPACE)))
+    time.sleep(120)
+    quilt_stop.wait()
+    quilt.terminate()
 
 def exp_iter(start, factor):
     while True:
         yield int(start)
         start *= factor
 
-def make_scale_process(count, fullmesh, arg):
+def format_specs(count):
     for spec in [PREBOOT_SPEC, FULL_MESH_BOOT_SPEC, FULL_MESH_POST_SPEC, BOOT_SPEC, POSTBOOT_SPEC]:
         with open(spec, "r") as read_file, open(spec + ".tmp", "w") as write_file:
             write_file.write(read_file.read(-1).format(str(count)))
 
+def make_scale_process(count, fullmesh, arg):
     if fullmesh:
         return subprocess.Popen(shlex.split(FULL_MESH_CMD + arg))
     return subprocess.Popen(shlex.split(DEFAULT_CMD + arg))
 
-def run_process(count, full_mesh, arg):
-    scale_process = make_scale_process(count, fullmesh, arg)
+def make_swarm_process(count, image, arg):
+    swarm_cmd_formatted = SWARM_CMD.format(PREBOOT_SPEC, image, count, OUT_FILE + ".swarm", LOG_FILE)
+    return subprocess.Popen(shlex.split(swarm_cmd_formatted + arg))
+
+def run_process(proc, count, opt, arg):
     try:
-        returncode = scale_process.wait()
-        if returncode != 0:
-            print("The scale tester exited with an error. Trying again")
-            time.sleep(30)
-            run_process(count, fullmesh, arg)
+        while True:
+            process = proc(count, opt, arg)
+            arg = ""
+            returncode = process.wait()
+            if returncode != 0:
+                print("The scale tester exited with an error. Trying again.")
+                stop_namespace() # stop the namespace so things restart
+            else:
+                return
     except KeyboardInterrupt:
-        scale_process.terminate()
+        process.terminate()
+        time.sleep(90) # wait to allow the process to finish its own shutdown
         sys.exit(0)
 
-def run_test(count, run_all, arg=""):
-    run_process(count, False, arg) # always run the disconnected test
-    if run_all:
-        run_process(count, True, arg) # run the full mesh test
-        run_swarm(count)        
+def run_test(count, run_all, image, arg=""):
+    format_specs(count)
+    run_process(make_scale_process, count, False, arg) # always run the disconnected test
+    if not run_all:
+        return
+    run_process(make_scale_process, count, True, "-ip-only") # run the full mesh test
+    run_process(make_swarm_process, count, image, "-ip-only")
 
 def run_scale(args):
     options = run_parser().parse_args(args)
     bootcounts = exp_iter(options.start, options.factor)
-    run_test(1, False) # boot one container to ensure that the machines are fully booted
+    run_test(1, False, "") # boot one container to ensure that the machines are fully booted
     if os.path.exists(LOG_FILE):
         os.remove(LOG_FILE)
     for count in bootcounts:
-        run_test(count, True, "-ip-only")
+        run_test(count, True, options.image, "-ip-only")
 
 def run(args):
     prog_name = args[0]
