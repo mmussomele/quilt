0233623bca51f5cc51442c900659151efb0a9c25
Matthew Mussomele <mmussomele@berkeley.edu>
2016-09-14 18:44:04 -0700

    util: Add loop timestamp logger for debugging
    Sometimes it's useful to get information on how often loops run
    and how often they take to run. This patch adds a convienient utility
    for doing that.

--- util/log.go
+++ util/log.go
@@ -27,3 +27,46 @@ func (f Formatter) Format(entry *log.Entry) ([]byte, error) {
 	b.WriteByte('\n')
 	return b.Bytes(), nil
 }
+
+// LoopTimeLogger is a utility struct that allows us to time how long loops take, as
+// well as how often they are triggered.
+type LoopTimeLogger struct {
+	loopname    string
+	lastStart   time.Time
+	lastEnd     time.Time
+	loopRunning bool
+}
+
+// NewLoopTimeLogger creates and returns a ready to use LoopTimeLogger
+func NewLoopTimeLogger(loopname string) *LoopTimeLogger {
+	return &LoopTimeLogger{
+		loopname:    loopname,
+		lastEnd:     time.Now(),
+		lastStart:   time.Time{},
+		loopRunning: false,
+	}
+}
+
+// LogLoopStart logs the start of a loop and how long it has been since the last trigger.
+func (ltl *LoopTimeLogger) LogLoopStart() {
+	if ltl.loopRunning {
+		return
+	}
+
+	ltl.loopRunning = true
+	ltl.lastStart = time.Now()
+	log.Debugf("Starting %s trigger loop. It has been %v "+
+		"since the last trigger.", ltl.loopname, ltl.lastStart.Sub(ltl.lastEnd))
+}
+
+// LogLoopEnd logs the end of a loop and how long it took to run.
+func (ltl *LoopTimeLogger) LogLoopEnd() {
+	if !ltl.loopRunning {
+		return
+	}
+
+	ltl.loopRunning = false
+	ltl.lastEnd = time.Now()
+	log.Debugf("%s trigger loop ended. It took %v",
+		ltl.loopname, ltl.lastEnd.Sub(ltl.lastStart))
+}
