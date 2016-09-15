210fd6ef6302532bb95be291dee3264608c76065
Matthew Mussomele <mmussomele@berkeley.edu>
2016-09-15 13:47:00 -0700

    sleep: Wait for network to come up before printing timestamp

--- quilt-tester/scale/sleep/scale-sleep.sh
+++ quilt-tester/scale/sleep/scale-sleep.sh
@@ -1,11 +1,18 @@
 #! /bin/sh
+set -e 
 
-timestamp() {
-    until ping -q -c1 localhost > /dev/null 2>&1; do
-        sleep 0.5
-    done
-    echo "quilt_timestamp=$(date -u +%s)"
-}
+# Wait until we can ping localhost
+until ping -q -c1 localhost > /dev/null 2>&1; do
+    sleep 0.5
+done
 
-timestamp
-sleep infinity
+# Wait for the network to come up
+while [ -z "$(ip address list eth0 | grep -e '^\s\s*inet\s\s*10\.\d\d*\.\d\d*\.\d\d*/8\s\s*.*$')" ]; do
+    sleep 0.5
+done
+
+# output the timestamp
+echo "quilt_timestamp=$(date -u +%s)"
+while true; do
+    sleep 60
+done
