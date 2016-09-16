80bc81d7ff35ae815c6fd0ae50914e2e25c1e504
Kevin Lin <kaikai526@gmail.com>
2016-09-15 23:48:32 -0700

    network: Optimize ACL rule syncing memory usage
    This patch revamps the way ACL rules are generated and stored to improve
    memory usage:
    1) The match string format is greatly shortened.  Before, we created a different
    ACL for each fromIP and toIP pair, resulting in lots of match strings of the form
    `(fromIP=="src1" && toIP=="dst")`.  This patch compresses the enumerated
    pairs into `(fromIP=="src1" || fromIP=="src2") && (toIP=="dst")`.
    2) The match string is lazily created during the Join from the desired
    connections, rather than creating all desired ACL before joining.
    3) Gets rid of the `from-lport` ACL direction and only imposes ACLs on
    `to-lport`.

--- minion/network/network_test.go
+++ minion/network/network_test.go
@@ -128,103 +128,29 @@ func TestACLUpdate(t *testing.T) {
 		MinPort: 80,
 		MaxPort: 81,
 	}
-	checkACLCount(t, client, []db.Connection{redBlueConnection}, 18)
+	checkACLCount(t, client, []db.Connection{redBlueConnection}, 2)
 	checkACLCount(t, client,
-		[]db.Connection{redBlueConnection, redYellowConnection}, 54)
-	checkACLCount(t, client, []db.Connection{redYellowConnection}, 42)
-	checkACLCount(t, client, nil, 2)
-}
-
-func allowMatch(acls map[ovsdb.AclCore]struct{}, match string) {
-	acls[ovsdb.AclCore{
-		Priority:  1,
-		Direction: "from-lport",
-		Action:    "allow",
-		Match:     match,
-	}] = struct{}{}
-	acls[ovsdb.AclCore{
-		Priority:  1,
-		Direction: "to-lport",
-		Action:    "allow",
-		Match:     match,
-	}] = struct{}{}
+		[]db.Connection{redBlueConnection, redYellowConnection}, 3)
+	checkACLCount(t, client, []db.Connection{redYellowConnection}, 2)
+	checkACLCount(t, client, nil, 1)
 }
 
 func TestACLGeneration(t *testing.T) {
-	exp := map[ovsdb.AclCore]struct{}{
-		{
-			Priority:  0,
-			Direction: "from-lport",
-			Match:     "ip",
-			Action:    "drop",
-		}: {},
-		{
-			Priority:  0,
-			Direction: "to-lport",
-			Match:     "ip",
-			Action:    "drop",
-		}: {},
-	}
-	allowMatch(exp, "ip4.src==8.8.8.8 && ip4.dst==9.9.9.9 "+
-		"&& (80 <= udp.dst <= 80 || 80 <= tcp.dst <= 80)")
-	allowMatch(exp, "ip4.src==8.8.8.8 && ip4.dst==9.9.9.9 && icmp")
-	allowMatch(exp, "ip4.src==9.9.9.9 && ip4.dst==8.8.8.8 "+
-		"&& (80 <= udp.src <= 80 || 80 <= tcp.src <= 80)")
-	allowMatch(exp, "ip4.src==9.9.9.9 && ip4.dst==8.8.8.8 && icmp")
-	allowMatch(exp, "ip4.src==10.10.10.10 && ip4.dst==12.12.12.12 "+
-		"&& (80 <= udp.dst <= 81 || 80 <= tcp.dst <= 81)")
-	allowMatch(exp, "ip4.src==10.10.10.10 && ip4.dst==12.12.12.12 && icmp")
-	allowMatch(exp, "ip4.src==10.10.10.10 && ip4.dst==13.13.13.13 "+
-		"&& (80 <= udp.dst <= 81 || 80 <= tcp.dst <= 81)")
-	allowMatch(exp, "ip4.src==10.10.10.10 && ip4.dst==13.13.13.13 && icmp")
-	allowMatch(exp, "ip4.src==11.11.11.11 && ip4.dst==12.12.12.12 "+
-		"&& (80 <= udp.dst <= 81 || 80 <= tcp.dst <= 81)")
-	allowMatch(exp, "ip4.src==11.11.11.11 && ip4.dst==12.12.12.12 && icmp")
-	allowMatch(exp, "ip4.src==11.11.11.11 && ip4.dst==13.13.13.13 "+
-		"&& (80 <= udp.dst <= 81 || 80 <= tcp.dst <= 81)")
-	allowMatch(exp, "ip4.src==11.11.11.11 && ip4.dst==13.13.13.13 && icmp")
-	allowMatch(exp, "ip4.src==12.12.12.12 && ip4.dst==10.10.10.10 "+
-		"&& (80 <= udp.src <= 81 || 80 <= tcp.src <= 81)")
-	allowMatch(exp, "ip4.src==12.12.12.12 && ip4.dst==10.10.10.10 && icmp")
-	allowMatch(exp, "ip4.src==12.12.12.12 && ip4.dst==11.11.11.11 "+
-		"&& (80 <= udp.src <= 81 || 80 <= tcp.src <= 81)")
-	allowMatch(exp, "ip4.src==12.12.12.12 && ip4.dst==11.11.11.11 && icmp")
-	allowMatch(exp, "ip4.src==13.13.13.13 && ip4.dst==10.10.10.10 "+
-		"&& (80 <= udp.src <= 81 || 80 <= tcp.src <= 81)")
-	allowMatch(exp, "ip4.src==13.13.13.13 && ip4.dst==10.10.10.10 && icmp")
-	allowMatch(exp, "ip4.src==13.13.13.13 && ip4.dst==11.11.11.11 "+
-		"&& (80 <= udp.src <= 81 || 80 <= tcp.src <= 81)")
-	allowMatch(exp, "ip4.src==13.13.13.13 && ip4.dst==11.11.11.11 && icmp")
+	testGenerator := newACLGenerator(allLabels)
 
-	actual := generateACLs(
-		[]aclConnection{
-			{
-				fromIPs: []string{"8.8.8.8"},
-				toIPs:   []string{"9.9.9.9"},
-				minPort: 80,
-				maxPort: 80,
-			},
-			{
-				fromIPs: []string{"10.10.10.10", "11.11.11.11"},
-				toIPs:   []string{"12.12.12.12", "13.13.13.13"},
-				minPort: 80,
-				maxPort: 81,
-			},
-		},
-	)
+	exp := acl{
+		match: "((((ip4.src==100.1.1.3) && (ip4.dst==100.1.1.1 || ip4.dst==100.1.1.2 || ip4.dst==13.13.13.13)) && (icmp || 80 <= udp.dst <= 81 || 80 <= tcp.dst <= 81)) || (((ip4.src==100.1.1.1 || ip4.src==100.1.1.2 || ip4.src==13.13.13.13) && (ip4.dst==100.1.1.3)) && (icmp || 80 <= udp.src <= 81 || 80 <= tcp.src <= 81)))",
+	}
+
+	actual := testGenerator.create(db.Connection{
+		From:    "yellow",
+		To:      "redBlue",
+		MinPort: 80,
+		MaxPort: 81,
+	})
 	if !reflect.DeepEqual(actual, exp) {
 		t.Errorf("Bad ACL generation: expected %v, got %v",
 			exp, actual)
-	}
-}
-
-func checkConnectionConstruction(t *testing.T, connections []db.Connection,
-	labels []db.Label, containers []db.Container, expected []aclConnection) {
-
-	actual := getACLConnections(connections, labels, containers)
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("Bad connection deconstruction: expected %v, got %v",
-			expected, actual)
 	}
 }
 
@@ -271,111 +197,3 @@ var yellowContainer = db.Container{
 	Labels: []string{"yellow"},
 }
 var allContainers = []db.Container{redContainer, blueContainer, yellowContainer}
-
-func TestConnectionBreakdown(t *testing.T) {
-
-	// No connections should result in no ACLs but the default drop rules.
-	checkConnectionConstruction(t,
-		[]db.Connection{}, []db.Label{}, []db.Container{}, nil)
-
-	// Test one connection (with range)
-	checkConnectionConstruction(t,
-		[]db.Connection{
-			{
-				From:    "red",
-				To:      "blue",
-				MinPort: 80,
-				MaxPort: 81,
-			},
-		},
-		allLabels,
-		allContainers,
-		[]aclConnection{
-			{
-				fromIPs: []string{redContainerIP},
-				toIPs:   []string{blueContainerIP, blueLabelIP},
-				minPort: 80,
-				maxPort: 81,
-			},
-		},
-	)
-
-	// Test connecting from label with multiple containers
-	checkConnectionConstruction(t,
-		[]db.Connection{
-			{
-				From:    "redBlue",
-				To:      "yellow",
-				MinPort: 80,
-				MaxPort: 80,
-			},
-		},
-		allLabels,
-		allContainers,
-		[]aclConnection{
-			{
-				fromIPs: []string{redContainerIP, blueContainerIP},
-				toIPs:   []string{yellowContainerIP, yellowLabelIP},
-				minPort: 80,
-				maxPort: 80,
-			},
-		},
-	)
-
-	// Test multiple connections
-	checkConnectionConstruction(t,
-		[]db.Connection{
-			{
-				From:    "redBlue",
-				To:      "yellow",
-				MinPort: 80,
-				MaxPort: 80,
-			},
-			{
-				From:    "red",
-				To:      "blue",
-				MinPort: 80,
-				MaxPort: 81,
-			},
-		},
-		allLabels,
-		allContainers,
-		[]aclConnection{
-			{
-				fromIPs: []string{redContainerIP, blueContainerIP},
-				toIPs:   []string{yellowContainerIP, yellowLabelIP},
-				minPort: 80,
-				maxPort: 80,
-			},
-			{
-				fromIPs: []string{redContainerIP},
-				toIPs:   []string{blueContainerIP, blueLabelIP},
-				minPort: 80,
-				maxPort: 81,
-			},
-		},
-	)
-
-	// Test toLabel with multiple containers
-	checkConnectionConstruction(t,
-		[]db.Connection{
-			{
-				From:    "yellow",
-				To:      "redBlue",
-				MinPort: 80,
-				MaxPort: 80,
-			},
-		},
-		allLabels,
-		allContainers,
-		[]aclConnection{
-			{
-				fromIPs: []string{yellowContainerIP},
-				toIPs: []string{redContainerIP, blueContainerIP,
-					redBlueLabelIP},
-				minPort: 80,
-				maxPort: 80,
-			},
-		},
-	)
-}
